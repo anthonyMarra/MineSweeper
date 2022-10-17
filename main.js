671b4294fbdd9f3705f190b55ddf056225110e26
anthonyMarra <marraanthony4@gmail.com>
2022-10-17 11:04:49 -0600

    Fix grid when clicked

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const tiles = document.querySelectorAll('.box')
 tiles.forEach(function (tile) {
     tile.addEventListener('click', clickTile)
 })
+function addClass(elem, cls) {
+    elem.className += (" " + cls);
+}
 
 function clickTile(tile) {
-    tile.target.className = "opened"
+    tile.target.className = tile.target.className + " opened"
+    console.log(tile.target.className)
 }
+
